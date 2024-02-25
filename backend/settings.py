a2e0c7313bd56027c10a4b7a727c1d465cefdf39
Jasurbek Xakimbekov <92104896+Jasurbek0424@users.noreply.github.com>
2024-02-25 22:12:18 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 

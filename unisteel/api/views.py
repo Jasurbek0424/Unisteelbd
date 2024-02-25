a003e6d5ea28e792c299959b86dfa101888d3550
Jasurbek <krasavchikxj@gmail.com>
2024-02-26 00:48:00 +0500

    zzz

--- unisteel/api/views.py
+++ unisteel/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from ..models import Product, Contacts, Rekviziti, CompaniyInfo, Servises
@@ -9,6 +10,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
 class ContactsViewSet(ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
 
